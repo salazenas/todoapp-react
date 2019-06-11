d01d29f689ec1a13a8c816aaa035317febc1410f
salazenas <salazenas@gmail.com>
2019-06-11 13:39:05 -0300

    First components created.

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Sobre</h1>
+        <p>App to keep activities and tasks under control.</p>
+    </div>
+)
---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
